8aa6aecc79361724aa9bb5ecc7d4bd1fb996b74d
Kian Kane <hooded_kian@yahoo.co.uk>
2016-11-24 15:27:56 +0000

    Added a map system

--- RogueSnakeGame/RogueSnakeGame/Game1.cs
+++ RogueSnakeGame/RogueSnakeGame/Game1.cs
@@ -2,61 +2,38 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using RogueSnakeGame.MapSystem;
+
 namespace RogueSnakeGame
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Map map;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            map = new Map(25, 15);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            map.LoadContent(Content);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -67,15 +44,13 @@ namespace RogueSnakeGame
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            map.Draw(spriteBatch);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ RogueSnakeGame/RogueSnakeGame/MapSystem/Map.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueSnakeGame.MapSystem
+{
+    class Map
+    {
+        private int width;
+        private int height;
+        private int tileWidth;
+        private int tileHeight;
+        private Tile[,] tiles;
+        private Texture2D floorTexture;
+        private Texture2D wallTexture;
+
+        public Map(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.tileWidth = 32;
+            this.tileHeight = 32;
+
+            tiles = new Tile[width, height];
+            FillMap(Tile.FLOOR);
+            FillEdges(Tile.WALL);
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            floorTexture = content.Load<Texture2D>("floor");
+            wallTexture = content.Load<Texture2D>("wall");
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Texture2D texture = floorTexture;
+                    switch (tiles[x, y])
+                    {
+                        case Tile.FLOOR:
+                            texture = floorTexture;
+                            break;
+                        case Tile.WALL:
+                            texture = wallTexture;
+                            break;
+                    }
+                    batch.Draw(texture, new Rectangle(x * tileWidth, y * tileHeight, tileWidth, tileHeight), Color.White);
+                }
+            }
+        }
+
+        public bool PlaceFree(int x, int y)
+        {
+            if (x < 0 || x >= width || y < 0 || y >= height) // Any tile which is out of bounds is considered impassable
+                return false;
+
+            return tiles[x, y] == Tile.FLOOR;
+        }
+
+        private void FillMap(Tile tile)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+
+        private void FillEdges(Tile tile, int borderWidth = 1)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (x < borderWidth || x >= width - borderWidth || y < borderWidth || y >= height - borderWidth)
+                        tiles[x, y] = tile;
+                }
+            }
+        }
+
+        public enum Tile
+        {
+            FLOOR,
+            WALL
+        }
+    }
+}

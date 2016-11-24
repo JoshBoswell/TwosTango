8507f88cbe1b81da0eab38059a8097c1bd056b08
Kian Kane <hooded_kian@yahoo.co.uk>
2016-11-24 22:15:00 +0000

    Added a utility class with a random function so that a single instance of Random can be used

---
+++ RogueSnakeGame/RogueSnakeGame/Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueSnakeGame
+{
+    class Util
+    {
+        private static Random random = new Random();
+
+        public static int Range(int min, int max)
+        {
+            return random.Next(max - min) + min;
+        }
+    }
+}
